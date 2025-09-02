99b225574c19b1bdd7906e09e9e1d7609d1f0ff8
Ade <ajibadeadewale68@gmail.com>
2025-09-02 23:48:37 +0100

    game

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -7,7 +7,7 @@ const HeroSection = () => {
     <section id='hero' className="hero-section">
       
       <div className="img1 floating-img slide-up">
-        <img src="../src/assets/overhead shot of use.png" alt="" />
+        <img src="./src/assets/overhead shot of use.png" alt="" />
       </div>
 
       <div className="hollow-square nav"></div>
@@ -15,7 +15,7 @@ const HeroSection = () => {
       <div className="hero-content">
         {/* <h1>WELCOME TO DANIEL ADEWALE .A.</h1> */}
         <p id='typing-text'>
-          Discover the expertise <br />
+          Discover the expertise of <br />
           Daniel Adewale .A.
         </p>
 
